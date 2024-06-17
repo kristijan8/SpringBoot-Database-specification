5d3b660a94b78fa79c3a81123d2ed2ad7b65e148
Kristijan Miloseski <kristijan.miloseski.f@gmail.com>
2024-06-17 14:59:13 +0200

    2

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.example.demo.StudentRepository;
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+
+
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository)
+    {
+        return args -> {
+            Student maria = new Student("Maria", "Jones", "maria1.jones@gmail.com", 21);
+            studentRepository.save(maria);
+        };
+
+    }
+
+}
